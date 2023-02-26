086fc09d44bfbbfcbd6ef697e72b4d3fc4c281f4
bencarneiro <bencarneiro@gmail.com>
2023-02-26 15:06:34 -0600

    add a test management command

---
+++ polygon/nft/management/commands/scrape_seaport_transactions.py
@@ -0,0 +1,13 @@
+from nft.models import SeaportTransaction
+
+
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **kwargs):
+        time = timezone.now().strftime('%X')
+        self.stdout.write("It's now %s" % time)
