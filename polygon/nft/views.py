74eb2a3bf2b2d37eb7b9e129767a8d8c5c447008
bencarneiro <bencarneiro@gmail.com>
2023-03-01 15:27:44 -0700

    add rough draft for daily sales calculation endpoint, add ability to filter by coin standard to get_transactions api endpoint

--- polygon/nft/views.py
+++ polygon/nft/views.py
@@ -33,12 +33,24 @@ def get_transactions(request):
          y &= Q(contract_address=request.GET['contract_address'])
     if "token_id" in request.GET:
         y &= Q(token_id=request.GET['token_id'])
+    if "coin_standard" in request.GET:
+        coin_standard = request.GET['coin_standard']
+        if coin_standard not in ['1155', '721']:
+            return JsonResponse({'status': 'error', 'message': 'coin_standard is invalid: needs to be 721 or 1155'})
+    else:
+        coin_standard = "all"
 
     data721 = Seaport721Transaction.objects.filter(q)
     data1155 = Seaport1155Transaction.objects.filter(q)
-    response_data = list(data721.values("tx_hash", "contract_address", "token_id", "buyer", "seller", "matic_price", "weth_price", "usdc_price", "tx_hash__dt", "tx_hash__block_number", "tx_hash__method_name")) + list(data1155.values("tx_hash", "contract_address", "token_id", "quantity", "buyer", "seller", "matic_price", "weth_price", "usdc_price", "tx_hash__dt", "tx_hash__block_number", "tx_hash__method_name"))
+    if coin_standard == "all":
+        response_data = list(data721.values("tx_hash", "contract_address", "token_id", "buyer", "seller", "matic_price", "weth_price", "usdc_price", "tx_hash__dt", "tx_hash__block_number", "tx_hash__method_name")) + list(data1155.values("tx_hash", "contract_address", "token_id", "quantity", "buyer", "seller", "matic_price", "weth_price", "usdc_price", "tx_hash__dt", "tx_hash__block_number", "tx_hash__method_name"))
+    if coin_standard == "721":
+        response_data = list(data721.values("tx_hash", "contract_address", "token_id", "buyer", "seller", "matic_price", "weth_price", "usdc_price", "tx_hash__dt", "tx_hash__block_number", "tx_hash__method_name"))
+    if coin_standard == "1155":
+        response_data = list(data1155.values("tx_hash", "contract_address", "token_id", "quantity", "buyer", "seller", "matic_price", "weth_price", "usdc_price", "tx_hash__dt", "tx_hash__block_number", "tx_hash__method_name"))
     
     response = {
+        'length': len(response_data),
         'status': "success",
         "data": response_data
     }
@@ -47,50 +59,56 @@ def get_transactions(request):
     
 
 @csrf_exempt
-def get_sales_by_contract(request):
-    return JsonResponse({})
-#     if "contract_address" in request.GET:
-#         contract_address = request.GET['contract_address']
-#     else:
-#         return JsonResponse({'error': "no contract address provided"})
-#     if "start_dt" in request.GET:
-#         start_dt = request.GET['start_dt']
-#     else:
-#         start_dt = None
-#     if "end_dt" in request.GET:
-#         end_dt = request.GET['end_dt']
-#     else:
-#         end_dt = None
-#     seaport_1155_txs = Seaport1155Transaction.objects.filter(
-#         contract_address=contract_address
-#     )
-#     seaport_721_txs = Seaport1155Transaction.objects.filter(
-#         contract_address=contract_address
-#     )
-#     if start_dt:
-#         seaport_1155_txs = seaport_1155_txs.filter(start_dt__date="2023-02-23")
-#     return JsonResponse({"data":[contract_address, token_id, start_dt, end_dt]})
+def get_daily_sales(request):
+    q = Q()
 
-# @csrf_exempt
-# def get_volume_by_token(request):
-#     contract_address = request.GET['contract_address']
-#     token_id = request.GET['token_id']
-#     start_dt = request.GET['start_dt']
-#     end_dt = request.GET['end_dt']
-#     return JsonResponse({"data":[contract_address, token_id, start_dt, end_dt]})
+    # these filters are run on the main tx table
 
-# @csrf_exempt
-# def get_sales_by_token_id(request):
-#     contract_address = request.GET['contract_address']
-#     token_id = request.GET['token_id']
-#     start_dt = request.GET['start_dt']
-#     end_dt = request.GET['end_dt']
-#     return JsonResponse({"data":[contract_address, token_id, start_dt, end_dt]})
+    if "start_dt" in request.GET:
+        q &= Q(tx_hash__dt__gte=datetime.fromtimestamp(int(request.GET['start_dt'])))
+    if "end_dt" in request.GET:
+        q &= Q(tx_hash__dt__lte=datetime.fromtimestamp(int(request.GET['end_dt'])))
+    if "start_block" in request.GET:
+        q &= Q(tx_hash__block_number__gte=request.GET['start_block'])
+    if "end_block" in request.GET:
+        q &= Q(tx_hash__block_number__lte=request.GET['end_block'])
 
-# @csrf_exempt
-# def get_volume_by_token_id(request):
-#     contract_address = request.GET['contract_address']
-#     token_id = request.GET['token_id']
-#     start_dt = request.GET['start_dt']
-#     end_dt = request.GET['end_dt']
-#     return JsonResponse({"data":[contract_address, token_id, start_dt, end_dt]})
+    # these filters are run on the subtable
+    if "buyer" in request.GET:
+        y &= Q(buyer=request.GET['buyer'])
+    if "seller" in request.GET:
+        y &= Q(seller=request.GET['seller'])
+    if "contract_address" in request.GET:
+         y &= Q(contract_address=request.GET['contract_address'])
+    if "token_id" in request.GET:
+        y &= Q(token_id=request.GET['token_id'])
+
+    data721 = Seaport721Transaction.objects.filter(q)
+    data1155 = Seaport1155Transaction.objects.filter(q)
+    # usdc_volume
+    response = {
+        "status": "success",
+        "data": []
+    }
+    return JsonResponse(response)
+
+    # steps
+    # take start_dt, end_dt, start_block, end_block
+    # segment the total timeframe into its component days by epoch   timestamp (49403394-3940494933)
+    # for each of the timeblocks, crunch the numbers on data721 and data1155
+    # add each set of stats to the data list in order
+
+
+
+    # todo allow for filter by type (721/1155)
+
+    # Structure needs to look like this
+    # {"data": [{
+    #     "date": "2020-15-15"
+    #     "matic_volume":
+    #     "weth_volume":
+    #     "usdc_volume"
+    #     "total volume in current USD"
+    #     "number of transactions"
+    #     "filter parameters" : {"contract_address": , "token_id"}
+    # }]}
--- polygon/polygon/urls.py
+++ polygon/polygon/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from nft.views import get_sales_by_contract, get_transactions
+from nft.views import get_daily_sales, get_transactions
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('get_sales_by_contract', get_sales_by_contract, name='get_sales_by_contract'),
+    path('get_daily_sales', get_daily_sales, name='get_daily_sales'),
     path('get_transactions', get_transactions, name='get_transactions'),
 ]
